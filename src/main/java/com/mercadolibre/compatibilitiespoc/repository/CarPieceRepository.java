303bb90dee0bf50751ad020e199b3adb62a3740f
Ignacio Garrone <nachogarrone@gmail.com>
2019-04-16 16:01:06 -0300

    repository

--- src/main/java/com/mercadolibre/compatibilitiespoc/repository/CarPieceRepository.java
+++ src/main/java/com/mercadolibre/compatibilitiespoc/repository/CarPieceRepository.java
@@ -1,12 +1,12 @@
 package com.mercadolibre.compatibilitiespoc.repository;
 
 import com.mercadolibre.compatibilitiespoc.model.CarPiece;
+import com.microsoft.spring.data.gremlin.repository.GremlinRepository;
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarPieceRepository extends CrudRepository<CarPiece, String> {
+public interface CarPieceRepository extends GremlinRepository<CarPiece, String> {
 
 	Optional<CarPiece> findByBrandAndMpn(String brand, String mpn);
 }
--- src/main/java/com/mercadolibre/compatibilitiespoc/repository/CarRepository.java
+++ src/main/java/com/mercadolibre/compatibilitiespoc/repository/CarRepository.java
@@ -1,13 +1,13 @@
 package com.mercadolibre.compatibilitiespoc.repository;
 
 import com.mercadolibre.compatibilitiespoc.model.Car;
+import com.microsoft.spring.data.gremlin.repository.GremlinRepository;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepository extends CrudRepository<Car, String> {
+public interface CarRepository extends GremlinRepository<Car, String> {
 
 	Optional<Car> findByBrandAndModelAndYearAndTrim(String brand, String model, int year, String trim);
 
